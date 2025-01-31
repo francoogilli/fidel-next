847f1267db83a2ba101ad94dca6a05b70b98390e
Franco Gilli <francogilli10@gmail.com>
2025-01-30 21:47:22 -0300

    Agregar contenedor para el componente PricingTable en la página de planes

--- src/app/planes/page.tsx
+++ src/app/planes/page.tsx
@@ -3,9 +3,9 @@ import PricingTable from "@/components/pricingTable";
 
 export default function Planes() {
   return (
-    <>
+    <div className="pb-32">
       <PricingCard viewComparison={false} />
       <PricingTable/>
-    </>
+    </div>
   );
 }
