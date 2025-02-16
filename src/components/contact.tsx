fd2887abde9a5a63423f2cbdc8667ac2cc6647ca
Franco Gilli <francogilli10@gmail.com>
2025-02-15 23:32:59 -0300

    Reemplazar el ícono de mensaje en el componente ContactSection con un SVG para mejorar la calidad visual

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -313,10 +313,26 @@ export default function ContactSection({ viewPage }: Props) {
                       className="w-full"
                       label="Rubro"
                       startContent={
-                        <MessageIcon
-                          className="size-[1rem] text-[#5e5e5e] mr-1"
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
                           strokeWidth="2"
-                        />
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="size-[1.1rem] text-[#5e5e5e] mr-1"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <path d="M3.5 5.5l1.5 1.5l2.5 -2.5" />
+                          <path d="M3.5 11.5l1.5 1.5l2.5 -2.5" />
+                          <path d="M3.5 17.5l1.5 1.5l2.5 -2.5" />
+                          <path d="M11 6l9 0" />
+                          <path d="M11 12l9 0" />
+                          <path d="M11 18l9 0" />
+                        </svg>
                       }
                       labelPlacement="outside"
                       placeholder="Selecciona tu rubro"
