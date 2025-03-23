a454e2d50c454c0a428977417d9482789632f3fc
Franco Gilli <francogilli10@gmail.com>
2025-03-22 23:17:05 -0300

    CAmbios

--- src/components/appmobile.tsx
+++ src/components/appmobile.tsx
@@ -78,9 +78,9 @@ export default function AppMobile() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     className="size-6 text-gray-600"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -131,9 +131,9 @@ export default function AppMobile() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     className="size-6 text-gray-600"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -247,9 +247,9 @@ export default function AppMobile() {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     className="size-6 text-gray-600"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
