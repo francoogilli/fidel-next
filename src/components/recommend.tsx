aeacc0783fa2024d1ccc11a9d001bfdad9154e27
Franco Gilli <francogilli10@gmail.com>
2025-03-26 20:17:08 -0300

    Cambio recomenda

--- src/components/recommend.tsx
+++ src/components/recommend.tsx
@@ -5,7 +5,6 @@ import UsersIcon from "../icons/users";
 import ThreeStripesLeft from "../icons/threeStripesLeft";
 import ThreeStripesRight from "../icons/threeStripesRight";
 import GradientLine from "./gradientLine";
-import Link from "next/link";
 
 export default function Recommend() {
   return (
@@ -26,165 +25,109 @@ export default function Recommend() {
         <GradientLine weight="1.5px" direction="right" />
       </div>
 
-      <div className="bg-gradient-to-r border border-[#ffebebd4] md:bg-gradient-to-br from-[#f3d1a424] via-[#FCFBF9] to-[#f3d1a424] max-w-[78.5rem] xl:max-w-[90rem] mx-4 xl:mx-auto mt-10 mb-10 px-4 space-y-7 rounded-3xl">
-        <div className="hidden lg:block relative p-10">
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-0 top-1/2 transform -translate-y-1/2 size-20"
-          />
-          <img
-            src="coin7.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-36 top-32 transform -translate-y-1/2 w-10 rotate-45"
-          />
-          <img
-            src="coin5.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-40 top-12 transform -translate-y-1/2 size-28"
-          />
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-20 top-52 transform -translate-y-1/2 w-12 blur-[4px] rotate-[20deg]"
-          />
-          <img
-            src="coin.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-44 top-[86%] transform -translate-y-1/2 size-24"
-          />
-          <img
-            src="coin2.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-44 top-10 transform -translate-y-1/2 size-28"
-          />
-          <img
-            src="coin82.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-10 top-20 rotate-[175deg] transform -translate-y-1/2 size-20"
-          />
-          <img
-            src="coin7.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-48 top-36 transform -translate-y-1/2 w-12"
-          />
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-10 top-52 transform rotate-[-20deg] -translate-y-1/2 size-16 blur-[4px]"
-          />
-          <img
-            src="coin3.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-44 top-[86%] transform -translate-y-1/2 size-24"
-          />
-          <p style={{ fontFamily: "Plus Jakarta Sans" }} className="text-3xl md:text-6xl xl:text-7xl  pb-5 font-extrabold text-center bg-gradient-to-br from-[#000000] to-[#434343] bg-clip-text text-transparent">
-            ¡Referi y gana!
-          </p>
-          <p className="font-medium text-[#434343] xl:text-lg text-center">
-            Recomenda <span className="font-bold text-black">Fidel</span> a tus
-            amigos y familiares.
-          </p>
-          <p className="font-medium text-[#434343] xl:text-lg text-center">
-            Gana <span className="font-bold text-black">$25.000</span> por cada
-            persona que se suscriba usando tu enlace.
-          </p>
-          <Link href="#">
-            <button className="flex justify-center items-center gap-x-2 mx-auto bg-[#212121] text-white font-normal rounded-[1.1rem] text-sm xl:text-base px-7 xl:px-8 py-3 mt-5 text-center border-4 border-[#FFF6D8] hover:border-[#ffc6c6] transition-all duration-700">
-              Descubri más
-              <ArrowUpIcon className="size-4" />
-            </button>
-          </Link>
+      <section className="py-16 hidden md:block">
+        <div className="container">
+          <div className="relative rounded-3xl overflow-hidden bg-muted/20 p-8 md:p-12 bg-[#FCF8F2] border border-[#fff0f0]">
+            <div className="relative w-full flex justify-center">
+              <div className="absolute -top-24 right-0 rotate-[30deg]">
+                <img src="/coin2.png" alt="" className="size-32" />
+              </div>
+              <div className="absolute -top-10 right-52 rotate-[30deg]">
+                <img src="/coin3.png" alt="" className="size-20 blur-[3px]" />
+              </div>
+              <div className="absolute top-36 right-24 rotate-[30deg]">
+                <img
+                  src="/coin3.png"
+                  alt=""
+                  className="size-24 -rotate-[30deg]"
+                />
+              </div>
+              <div className="absolute top-32 -right-14 rotate-[30deg]">
+                <img
+                  src="/coin4.png"
+                  alt=""
+                  className="size-20 -rotate-[50deg]"
+                />
+              </div>
+              <div className="absolute -top-7 right-[21rem] -rotate-[199deg]">
+                <img
+                  src="/coin4.png"
+                  alt=""
+                  className="size-20 -rotate-[50deg]"
+                />
+              </div>
+              <div className="absolute -top-7 right-[28rem] rotate-[190deg]">
+                <img
+                  src="/coin5.png"
+                  alt=""
+                  className="size-20 -rotate-[50deg] blur-[5px]"
+                />
+              </div>
+              <div className="absolute top-36 right-[2rem] rotate-[240deg]">
+                <img
+                  src="/coin4.png"
+                  alt=""
+                  className="size-10 -rotate-[50deg] blur-[3px]"
+                />
+              </div>
+              <div className="absolute top-24 right-60 rotate-[30deg]">
+                <img
+                  src="/coin5.png"
+                  alt=""
+                  className="size-28 -rotate-[30deg]"
+                />
+              </div>
+              <div className="absolute top-24 right-[25rem] rotate-[30deg]">
+                <img
+                  src="/coin.png"
+                  alt=""
+                  className="size-[5.5rem] -rotate-[30deg] blur-[3px]"
+                />
+              </div>
+            </div>
+
+            {/* Gradiente de fondo */}
+            <div className="absolute inset-0 bg-gradient-to-r from-muted/50 to-transparent"></div>
+
+            {/* Shadow blanco hacia adentro de izquierda a derecha */}
+            <div className="absolute inset-0 pointer-events-none before:absolute before:inset-0 before:bg-gradient-to-r before:from-[#FCF8F2] before:via-[#FCF8F2] before:to-transparent"></div>
+
+            <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-8">
+              <div>
+                <p className="text-sm text-muted-foreground mb-2 px-4 py-1.5 border border-[#2121210c] rounded-full inline-block bg-[#6666660c]">
+                  ¡Novedad!
+                </p>
+                <h2 className="text-2xl pb-2 font-sans bg-gradient-to-br from-[#000000] to-[#434343] bg-clip-text text-transparent md:text-6xl font-extrabold mb-4 ">
+                  ¡Recomenda y gana!
+                </h2>
+                <p className="text-sm md:text-base flex justify-start gap-x-1.5 text-muted-foreground max-w-3xl">
+                  Recomenda{" "}
+                  <span className="inline-flex items-center space-x-1">
+                    <img
+                      src="/fidel1.svg"
+                      className="w-6"
+                      draggable="false"
+                      alt="Logo Fidel"
+                    />
+                    <p className="font-medium">Fidel</p>
+                  </span>{" "}
+                  a tus amigos y familiares.
+                </p>
+
+                <p className="text-sm md:text-base text-muted-foreground max-w-xl">
+                  Gana <span className="font-bold">$25.000</span> por cada
+                  persona que se suscriba usando tu enlace.
+                </p>
+              </div>
+              <button className="flex font-sans font-medium justify-center items-center gap-x-2 px-6 py-3 rounded-2xl bg-gradient-to-l from-[#191919] to-[#414141] text-white">
+                Descubrir más
+                <ArrowUpIcon className="size-4" />
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="hidden md:block lg:hidden relative p-8">
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-0 top-24 transform -translate-y-1/2 size-10"
-          />
-          <img
-            src="coin7.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-16 top-24 transform -translate-y-1/2 w-6 rotate-45"
-          />
-          <img
-            src="coin5.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-16 top-7 transform -translate-y-1/2 size-[4.5rem]"
-          />
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-6 top-36 transform -translate-y-1/2 w-8 blur-[4px] rotate-[20deg]"
-          />
-          <img
-            src="coin.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute left-14 -bottom-11 transform -translate-y-1/2 size-[4.5rem]"
-          />
-          <img
-            src="coin2.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-16 top-6 transform -translate-y-1/2 size-[4.5rem]"
-          />
-          <img
-            src="coin82.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-0 top-16 rotate-[175deg] transform -translate-y-1/2 size-12"
-          />
-          <img
-            src="coin7.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-0 top-32 transform -translate-y-1/2 w-8"
-          />
-          <img
-            src="coin4.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-10 top-32 transform rotate-[-20deg] -translate-y-1/2 size-7 blur-[4px]"
-          />
-          <img
-            src="coin3.png"
-            alt="Moneda"
-            draggable="false"
-            className="select-none absolute right-9 -bottom-12 transform -translate-y-1/2 size-[4.5rem]"
-          />
-          <p className="text-5xl pb-5 font-bold text-center bg-gradient-to-br from-[#000000] to-[#434343] bg-clip-text text-transparent">
-            ¡Referi y gana!
-          </p>
-          <p className="font-medium text-sm text-[#434343] text-center">
-            Recomenda <span className="font-bold text-black">Fidel</span> a tus
-            amigos y familiares.
-          </p>
-          <p className="font-medium text-sm text-[#434343] text-center">
-            Gana <span className="font-bold text-black">$25.000</span> por cada
-            persona que se suscriba usando tu enlace.
-          </p>
-          <Link href="#">
-            <button className="flex justify-center items-center mx-auto text-xs bg-[#212121] gap-x-1.5  text-white font-normal rounded-2xl px-7 py-3 mt-5 text-center">
-              Descubri más
-              <ArrowUpIcon className="size-4" />
-            </button>
-          </Link>
-        </div>
+      </section>
+      <div className="bg-[#FCF8F2] border border-[#ffebebd4] md:bg-gradient-to-br max-w-[78.5rem] xl:max-w-[90rem] mx-4 xl:mx-auto mt-10 mb-10 px-4 space-y-7 rounded-3xl">
         <div className="block md:hidden relative py-4">
           <img
             src="coin.png"
@@ -252,19 +195,31 @@ export default function Recommend() {
             draggable="false"
             className="select-none absolute -left-1 top-28 transform -translate-y-1/2 size-8"
           />
-          <p className="text-2xl pb-3.5 font-bold text-center bg-gradient-to-br from-[#000000] to-[#434343] bg-clip-text text-transparent">
-            ¡Referi y gana!
+          <p className="text-2xl pb-3.5 font-sans font-extrabold text-center bg-gradient-to-br from-[#000000] to-[#434343] bg-clip-text text-transparent">
+            ¡Recomenda y gana!
           </p>
-          <p className="font-medium text-xs text-[#434343] text-center pb-2">
-            Gana <span className="font-bold text-black">$25.000</span> por cada
-            persona que se suscriba usando tu enlace.
+          <p className="text-xs md:text-base flex justify-start gap-x-1.5 text-muted-foreground max-w-3xl">
+            Recomenda{" "}
+            <span className="inline-flex items-center space-x-1">
+              <img
+                src="/fidel1.svg"
+                className="w-4"
+                draggable="false"
+                alt="Logo Fidel"
+              />
+              <p className="font-medium">Fidel</p>
+            </span>{" "}
+            a tus amigos y familiares.
           </p>
-          <Link href="#">
-            <button className="flex justify-center items-center mx-auto px-7 py-4 mt-5 w-full text-sm bg-[#212121] text-white gap-x-1.5 font-normal rounded-[0.875rem] text-center">
-              Descubri más
-              <ArrowUpIcon className="size-4" />
-            </button>
-          </Link>
+
+          <p className="text-xs md:text-base text-muted-foreground max-w-xl">
+            Gana <span className="font-bold">$25.000</span> por cada persona que
+            se suscriba usando tu enlace.
+          </p>
+          <button className="flex justify-center items-center mx-auto px-7 py-4 mt-5 w-full text-sm bg-[#212121] text-white gap-x-1.5 font-normal rounded-[0.875rem] text-center">
+            Descubri más
+            <ArrowUpIcon className="size-4" />
+          </button>
         </div>
       </div>
     </div>
