20994e994b52038ac853cb103f3abc0c0cd92e6a
Franco Gilli <francogilli10@gmail.com>
2025-06-29 09:44:47 -0300

    Actualizar el botón en el componente Hero para usar un degradado de fondo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -55,7 +55,7 @@ export default function Hero() {
           >
             <div className="flex flex-col md:flex-row md:space-x-3 mx-auto lg:mx-0 justify-center lg:justify-start items-center text-center space-y-2 md:space-y-0">
               <motion.button
-                className="bg-[#5fff5c] hover:bg-[#31de2e] tracking-tighter md:tracking-normal text-[#171717] justify-center w-auto gap-2 font-bold py-2.5 px-6 md:px-7 md:py-3 text-base md:text-[15px] rounded-[18px] flex items-center space-x-2 border-4 md:border-[5px] border-[#fafafa] hover:border-[#dfffdf] transition-all duration-700"
+                className="bg-gradient-to-br from-[#6fe95f] via-[#b2fa9e] to-[#6fe95f] tracking-tighter md:tracking-normal text-[#171717] justify-center w-auto gap-2 font-bold py-2.5 px-6 md:px-7 md:py-3 text-base md:text-[15px] rounded-[18px] flex items-center space-x-2 border-4 md:border-[5px] border-[#fafafa] hover:border-[#dfffdf] transition-all duration-700"
                 style={{ fontFamily: "Plus Jakarta Sans" }}
               >
                 <SparklesIcon className="size-4 md:size-5" />
