d5112c830378828eb24fc3e6f71fe94d46bce8a3
Franco Gilli <franco.gilli@trizap.net>
2025-10-23 16:32:18 -0300

    Cambio

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -116,8 +116,23 @@ export default function Header() {
                       href="#"
                     >
                       <div className="relative flex items-center gap-2 w-full">
-                        <span className="text-white text-sm group-hover:text-white  ">
-                          Ingresar
+                        <span className="text-white text-sm group-hover:text-white flex items-center gap-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="size-4"
+                          >
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                            <circle cx="12" cy="7" r="4" />
+                          </svg>
+                          Iniciar sesión
                         </span>
                       </div>
                     </Link>
