2cd8513bef8eae6a8fe9c2f9b6b639f100dae5d8
Franco Gilli <franco.gilli@trizap.net>
2025-10-30 10:14:34 -0300

    Cambios

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -132,9 +132,9 @@ export default function Header() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="size-4"
                           >
                             <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
